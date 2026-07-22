e55d1cabf059281fb672475699db5ef81bb19fba
andrei.cristea <andrei.cristea@qdrant.com>
2026-07-22 13:52:42 +0200

    feat: added singals and targets for the datasets

--- src/report_charts/__init__.py
+++ src/report_charts/__init__.py
@@ -8,20 +8,43 @@ that prefer one-shot invocation.
 """
 
 from ._base import DOMAIN_PALETTE, ROUTER_SIGNAL_AXES
+from .coverage_map import (
+    DEFAULT_AXES,
+    CoverageAxis,
+    CoverageMap,
+    SpanCountAxis,
+    StatAxis,
+    coverage_map,
+)
+from .coverage_map_grid import CoverageMapGrid, coverage_map_grid
 from .domain_donut import feature_donut
+from .equal_weight import EqualWeightScale
 from .fingerprint_heatmap import FingerprintHeatmap, fingerprint_heatmap
 from .fingerprint_spider import FingerprintSpider, fingerprint_spider
+from .fingerprint_spider_grid import FingerprintSpiderGrid
 from .share_bar import domain_share_bar
 from .signal_bars import signal_bars
+from .target_composition import target_composition
 
 __all__ = [
+    "DEFAULT_AXES",
     "DOMAIN_PALETTE",
+    "CoverageAxis",
+    "CoverageMap",
+    "CoverageMapGrid",
+    "EqualWeightScale",
     "FingerprintHeatmap",
     "FingerprintSpider",
+    "FingerprintSpiderGrid",
     "ROUTER_SIGNAL_AXES",
+    "SpanCountAxis",
+    "StatAxis",
+    "coverage_map",
+    "coverage_map_grid",
     "domain_share_bar",
     "feature_donut",
     "fingerprint_heatmap",
     "fingerprint_spider",
     "signal_bars",
+    "target_composition",
 ]
